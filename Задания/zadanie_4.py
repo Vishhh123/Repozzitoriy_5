7ed38fe478610061997edd4b29049358f02c71fc
Fedorova_Darya <fdu12345@mail.ru>
2025-12-25 01:19:06 +0300

    Код к заданию 4

---
+++ Задания/zadanie_4.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+
+    slovar1 = {1: 'one', 2: 'two', 3: 'three'}
+
+    print(slovar1)
+
+    items_object = slovar1.items()
+
+    print(items_object)
+
+    slovar2 = {v: k for k, v in items_object}
+
+    print(slovar2)
+
+    
