64f4527ae9c136d3d72c12244bb848feffe20261
Fedorova_Darya <fdu12345@mail.ru>
2025-12-25 00:58:05 +0300

    Код к заданию 2

--- Задания/zadanie_1.py
+++ Задания/zadanie_1.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 if __name__ == "__main__":
+
     alfavit_glasn = set("aeiouyаеёиоуыэюяAEIOUYАЕЁИОУЫЭЮЯ")
 
     text = input("Введите строку: ")
@@ -17,8 +18,9 @@ if __name__ == "__main__":
             count += 1
 
     print(f"Найдено гласных букв: {count}")
-
     print(f"Уникальные гласные в строке: {peresechenie}")
+
+
 
 
 
